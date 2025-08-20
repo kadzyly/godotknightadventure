93e4e20b57eab26ec3c2ce92be3ebb569f3fbe3b
KadzyLy <kadzyly@gmail.com>
2025-08-20 20:12:11 +0200

    feat: add movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var speed = 400
 
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
